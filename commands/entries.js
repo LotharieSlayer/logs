c811bb9877d8349fe33f5f8b804980ed07c5c717
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2023-01-05 23:47:32 +0100

    Correction d'une erreur d'importation

--- commands/entries.js
+++ commands/entries.js
@@ -26,7 +26,7 @@ const slashCommand = new SlashCommandBuilder()
 async function execute(interaction) {
 
     const logsFile = new AttachmentBuilder(
-        `../files/userEntries.log`
+        `${__dirname}/../files/userEntries.log`
     );
     await interaction.member.send({
         content: `Logs des entrées de membres du serveur ${interaction.guild.name} (${interaction.guildId}).`,
